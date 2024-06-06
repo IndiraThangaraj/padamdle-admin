232c2c85b6c927438a74726b5632042d6bbff1d9
IndiraThangaraj <indira02032001@gmail.com>
2024-06-06 13:45:47 +0800

    new changes

--- src/Table.js
+++ src/Table.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Zoom from 'react-medium-image-zoom';
-import 'react-medium-image-zoom/dist/styles.css';
-import './Table.css'
+import React, { useState, useEffect } from "react";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+import "./Table.css";
 
 const Table = () => {
   const [rows, setRows] = useState([]);
@@ -10,23 +10,25 @@ const Table = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://httpsflaskexample-wmsssx4hca-uc.a.run.app/padamdleadmin/randomtamilmovies?count=10');
+        const response = await fetch(
+          "https://httpsflaskexample-wmsssx4hca-uc.a.run.app/padamdleadmin/movies"
+        );
         const data = await response.json();
-        const formattedData = data.map((item, index) => ({
-          id: index + 1,
-          col1: index + 1, // Serial No
-          col2: item.name, //movie name
-          col3: '', // IMDB 
-          col4: item.year, 
-          col5: '', //Synopsis
-          col6: Array(6).fill(''), // Images (array of 6)
-          col7: null, //  Audio file input
-          col8: null, // Video file input
-          col9: Array(6).fill(''), // Youtube Videos array input
+        const formattedData = Object.keys(data).map((key, index) => ({
+          id: parseInt(key),
+          col1: key, // Serial No
+          col2: data[key].name, // Movie name
+          col3: data[key].rating, // IMDB
+          col4: data[key].year, // Year
+          col5: data[key].synopsis, // Synopsis
+          col6: data[key].images, // Images (array of 6)
+          col7: data[key].song, // Audio file input
+          col8: data[key].video, // Video file input
+          col9: data[key].yt.split(","), // Youtube Videos array input
         }));
         setRows(formattedData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -34,39 +36,45 @@ const Table = () => {
   }, []);
 
   const deleteRow = (id) => {
-    setRows(rows.filter(row => row.id !== id));
+    setRows(rows.filter((row) => row.id !== id));
   };
 
   const handleInputChange = (id, column, index, value) => {
-    setRows(rows.map(row => {
-      if (row.id === id) {
-        if (Array.isArray(row[column])) {
-          return {
-            ...row,
-            [column]: row[column].map((item, i) => i === index ? value : item)
-          };
-        } else {
-          return {
-            ...row,
-            [column]: value
-          };
+    setRows(
+      rows.map((row) => {
+        if (row.id === id) {
+          if (Array.isArray(row[column])) {
+            return {
+              ...row,
+              [column]: row[column].map((item, i) =>
+                i === index ? value : item
+              ),
+            };
+          } else {
+            return {
+              ...row,
+              [column]: value,
+            };
+          }
         }
-      }
-      return row;
-    }));
+        return row;
+      })
+    );
   };
 
   const handleFileChange = (id, column, file) => {
-    setRows(rows.map(row => row.id === id ? { ...row, [column]: file } : row));
+    setRows(
+      rows.map((row) => (row.id === id ? { ...row, [column]: file } : row))
+    );
   };
 
   const toggleExpand = (id, column) => {
-    setExpandedRows(prevState => ({
+    setExpandedRows((prevState) => ({
       ...prevState,
       [column]: {
         ...prevState[column],
-        [id]: !prevState[column][id]
-      }
+        [id]: !prevState[column][id],
+      },
     }));
   };
 
@@ -87,84 +95,152 @@ const Table = () => {
         </tr>
       </thead>
       <tbody>
-        {rows.map(row => (
+        {rows.map((row) => (
           <tr key={row.id}>
             <td>{row.col1}</td>
             <td>{row.col2}</td>
             <td>
-              <input 
-                type="text" 
-                value={row.col3} 
-                onChange={(e) => handleInputChange(row.id, 'col3', 0, e.target.value)} 
+              <input
+                type="text"
+                value={row.col3}
+                onChange={(e) =>
+                  handleInputChange(row.id, "col3", 0, e.target.value)
+                }
               />
             </td>
             <td>{row.col4}</td>
             <td>
-              <input 
-                type="text" 
-                value={row.col5} 
-                onChange={(e) => handleInputChange(row.id, 'col5', 0, e.target.value)} 
+              <input
+                type="text"
+                value={row.col5}
+                onChange={(e) =>
+                  handleInputChange(row.id, "col5", 0, e.target.value)
+                }
               />
             </td>
             <td>
-              <div onClick={() => toggleExpand(row.id, 'col6')} style={{ cursor: 'pointer' }}>
-                {expandedRows.col6[row.id] ? 'Done' : 'Add'}
+              <div>
+                {expandedRows.col6[row.id] ? (
+                  <>
+                    {row.col6 &&
+                      row.col6.map((val, index) => (
+                        <div key={index}>
+                          <Zoom>
+                            <img
+                              src={val}
+                              alt={`Image ${index + 1}`}
+                              style={{
+                                width: "100px",
+                                height: "auto",
+                                margin: "5px",
+                              }}
+                            />
+                          </Zoom>
+                        </div>
+                      ))}
+                    <button onClick={() => toggleExpand(row.id, "col6")}>
+                      Collapse
+                    </button>
+                  </>
+                ) : (
+                  <button onClick={() => toggleExpand(row.id, "col6")}>
+                    View
+                  </button>
+                )}
               </div>
-              {expandedRows.col6[row.id] && (
-                <div>
-                  {row.col6.map((val, index) => (
-                    <div key={index}>
-                      <input 
-                        type="text" 
-                        value={val} 
-                        onChange={(e) => handleInputChange(row.id, 'col6', index, e.target.value)} 
-                      />
-                      {val && (
-                        <Zoom>
-                          <img 
-                            src={val} 
-                            alt={`Image ${index + 1}`} 
-                            style={{ width: '100px', height: 'auto', margin: '5px' }}
-                          />
-                        </Zoom>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              )}
             </td>
             <td>
-              <input 
-                type="file" 
-                accept=".mp3" 
-                onChange={(e) => handleFileChange(row.id, 'col7', e.target.files[0])} 
-              />
-            </td>
-            <td>
-              <input 
-                type="file" 
-                accept=".mp4" 
-                onChange={(e) => handleFileChange(row.id, 'col8', e.target.files[0])} 
-              />
-            </td>
-            <td>
-              <div onClick={() => toggleExpand(row.id, 'col9')} style={{ cursor: 'pointer' }}>
-                {expandedRows.col9[row.id] ? 'Done' : 'Add'}
-              </div>
-              {expandedRows.col9[row.id] && (
-                <div>
-                  {row.col9.map((val, index) => (
-                    <input 
-                      key={index} 
-                      type="text" 
-                      value={val} 
-                      onChange={(e) => handleInputChange(row.id, 'col9', index, e.target.value)} 
+              <div>
+                {row.col7 && (
+                  <>
+                    <audio
+                      controls
+                      src={`https://firebasestorage.googleapis.com/v0/b/padamdle.appspot.com/o/${row.col7}.mp3?alt=media`}
                     />
-                  ))}
-                </div>
-              )}
+                    <br />
+                  </>
+                )}
+                <input
+                  type="file"
+                  accept=".mp3"
+                  onChange={(e) =>
+                    handleFileChange(row.id, "col7", e.target.files[0])
+                  }
+                />
+              </div>
             </td>
-            <td><button onClick={() => deleteRow(row.id)}>Delete</button></td>
+
+            <td>
+              <div>
+                {row.col8 && (
+                  <>
+                    <video
+                      controls
+                      src={`https://firebasestorage.googleapis.com/v0/b/padamdle.appspot.com/o/${row.col8}.mp4?alt=media`}
+                    />
+                    <br />
+                  </>
+                )}
+                <input
+                  type="file"
+                  accept=".mp4"
+                  onChange={(e) =>
+                    handleFileChange(row.id, "col8", e.target.files[0])
+                  }
+                />
+              </div>
+            </td>
+
+            <td>
+              <div>
+                {expandedRows.col9[row.id] ? (
+                  <>
+                    {row.col9 &&
+                      row.col9.map((val, index) => (
+                        <div key={index}>
+                          <a
+                            href={`https://www.youtube.com/watch?v=${val}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {`https://www.youtube.com/watch?v=${val}`}
+                          </a>
+                          <input
+                            type="text"
+                            value={val}
+                            onChange={(e) =>
+                              handleInputChange(
+                                row.id,
+                                "col9",
+                                index,
+                                e.target.value
+                              )
+                            }
+                          />
+                        </div>
+                      ))}
+                    <button onClick={() => toggleExpand(row.id, "col9")}>
+                      Collapse
+                    </button>
+                    <button
+                      onClick={() =>
+                        handleInputChange(row.id, "col9", row.col9.length, "")
+                      }
+                    >
+                      Add
+                    </button>
+                  </>
+                ) : (
+                  <button onClick={() => toggleExpand(row.id, "col9")}>
+                    View
+                  </button>
+                )}
+              </div>
+            </td>
+
+            <td>
+              <button onClick={() => deleteRow(row.id)}>Delete</button>
+            </td>
           </tr>
         ))}
       </tbody>
